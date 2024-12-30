23e03b7f65378e58c643431db6934d63b2b8f5bb
fab <fabrizio.salmi@gmail.com>
2024-12-30 11:10:21 +0100

    Update generate_squid_conf.sh

--- squid-config/generate_squid_conf.sh
+++ squid-config/generate_squid_conf.sh
@@ -37,7 +37,6 @@ download_and_merge_blocklists() {
     done < <(jq -c ".blocklists.${blocklist_type}.sources[]" /config.yaml)
     echo "${blocklist_type} blocklist merging finished"
 }
-
 # Process config.yaml
 CONFIG_YAML="/config.yaml"
 if [ ! -f "$CONFIG_YAML" ]; then
@@ -51,14 +50,43 @@ apk --no-cache add jq
 download_and_merge_blocklists "dns" "$(jq -r '.blocklists.dns.local_file' "$CONFIG_YAML")"
 download_and_merge_blocklists "ip" "$(jq -r '.blocklists.ip.local_file' "$CONFIG_YAML")"
 
+#override config with env variables
+set -a # Export all variables as environment variables
+if [[ ! -z "$SQUID_PORT" ]]; then export SQUID_PORT=$(echo "$SQUID_PORT" | sed -E 's/[^0-9]+//g'); fi
+if [[ ! -z "$SQUID_CACHE_SIZE" ]]; then export SQUID_CACHE_SIZE=$(echo "$SQUID_CACHE_SIZE" | sed -E 's/[^0-9]+//g'); fi
 
+if [[ ! -z "$SQUID_EXTERNAL_DNS_ENABLED" ]]; then export SQUID_EXTERNAL_DNS_ENABLED=$(echo "$SQUID_EXTERNAL_DNS_ENABLED" | sed -E 's/[^a-z]+//ig'); fi
+if [[ ! -z "$SQUID_EXTERNAL_DNS_RESOLVERS" ]]; then export SQUID_EXTERNAL_DNS_RESOLVERS=$(echo "$SQUID_EXTERNAL_DNS_RESOLVERS"  | sed -E 's/(,$)|(,,+)/,/g;s/^,|,$//g'); fi
+
+if [[ ! -z "$SQUID_HTTPS_ENABLED" ]]; then export SQUID_HTTPS_ENABLED=$(echo "$SQUID_HTTPS_ENABLED" | sed -E 's/[^a-z]+//ig'); fi
+if [[ ! -z "$SQUID_HTTPS_PORT" ]]; then export SQUID_HTTPS_PORT=$(echo "$SQUID_HTTPS_PORT" | sed -E 's/[^0-9]+//g'); fi
+if [[ ! -z "$SQUID_HTTPS_SSL_CERT" ]]; then export SQUID_HTTPS_SSL_CERT="$SQUID_HTTPS_SSL_CERT"; fi
+if [[ ! -z "$SQUID_HTTPS_SSL_KEY" ]]; then export SQUID_HTTPS_SSL_KEY="$SQUID_HTTPS_SSL_KEY"; fi
+
+if [[ ! -z "$BLOCKLISTS_DNS_ENABLED" ]]; then export BLOCKLISTS_DNS_ENABLED=$(echo "$BLOCKLISTS_DNS_ENABLED" | sed -E 's/[^a-z]+//ig'); fi
+if [[ ! -z "$BLOCKLISTS_IP_ENABLED" ]]; then export BLOCKLISTS_IP_ENABLED=$(echo "$BLOCKLISTS_IP_ENABLED" | sed -E 's/[^a-z]+//ig'); fi
+
+if [[ ! -z "$WHITELIST_ENABLED" ]]; then export WHITELIST_ENABLED=$(echo "$WHITELIST_ENABLED" | sed -E 's/[^a-z]+//ig'); fi
+if [[ ! -z "$WHITELIST_IPS" ]]; then export WHITELIST_IPS=$(echo "$WHITELIST_IPS"  | sed -E 's/(,$)|(,,+)/,/g;s/^,|,$//g'); fi
+if [[ ! -z "$WHITELIST_DOMAIN_ALLOWLIST" ]]; then export WHITELIST_DOMAIN_ALLOWLIST=$(echo "$WHITELIST_DOMAIN_ALLOWLIST" | sed -E 's/(,$)|(,,+)/,/g;s/^,|,$//g'); fi
+
+if [[ ! -z "$NETWORK_ALLOW_DIRECT" ]]; then export NETWORK_ALLOW_DIRECT=$(echo "$NETWORK_ALLOW_DIRECT" | sed -E 's/[^a-z]+//ig'); fi
+if [[ ! -z "$AUTHENTICATION_ENABLED" ]]; then export AUTHENTICATION_ENABLED=$(echo "$AUTHENTICATION_ENABLED" | sed -E 's/[^a-z]+//ig'); fi
+if [[ ! -z "$AUTHENTICATION_USERS" ]]; then export AUTHENTICATION_USERS="$AUTHENTICATION_USERS"; fi
+
+set +a
 # generate the squid config file
 # jq needs to be installed in the dockerfile
 envsubst < /squid-config/squid.conf.template  > /etc/squid/squid.conf
 
 # Generate user file for authentication
 if [[ "$(jq -r '.authentication.enabled' "$CONFIG_YAML")" == "true" ]] ; then
+	if [[ ! -z "$AUTHENTICATION_USERS" ]]; then
+		 echo "Generating user authentication file using environment variables"
+       echo "$AUTHENTICATION_USERS"  | sed 's/,/\n/g'| awk -F: '{print $1":$(openssl passwd -crypt "$2")"}'  > /etc/squid/users
+	 else
         echo "Generating user authentication file"
         awk -F: '{print $1":$(openssl passwd -crypt "$2")"}' /config.yaml | awk '/^users:/ {n=1;next} n {print}' | grep -v '^$' > /etc/squid/users
+	 fi
 fi
 echo "Squid configuration generated successfully!"
